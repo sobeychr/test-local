efb9fcff8e1f50ddcc20b35a760d4f63948a501f
sobeychr <sobey.christopher@gmail.com>
2021-02-17 17:41:22 -0500

    first commit

---
+++ client/index.jsx
@@ -0,0 +1,5 @@
+const ttt = () => {
+    const onClick = (e) => console.log('ttt', e);
+
+    return <a onClick={onClick}>ttt link</a>;
+};
---
+++ webpack/index.jsx
@@ -0,0 +1,8 @@
+const path = require('path');
+
+const pathRoot = path.resolve(__dirname, './../').concat('/');
+const pathClient = pathRoot.concat('client/');
+const pathDist = pathRoot.concat('dist/');
+const pathSever = pathRoot.concat('server/');
+
+module.exports = {};
