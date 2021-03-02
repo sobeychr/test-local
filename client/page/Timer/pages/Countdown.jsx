24cb7f043706f70869c910b9c7df4cc7fc7a5682
sobeychr <sobey.christopher@gmail.com>
2021-03-02 17:05:19 -0500

    +Added InputText component +Added Countdown content +Added util/time

--- client/page/Timer/pages/Countdown.jsx
+++ client/page/Timer/pages/Countdown.jsx
@@ -1,5 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Form, FormControl, InputGroup, Row } from 'react-bootstrap';
 
-const Countdown = () => <h1>Countdown</h1>;
+import Input from '@component/InputText/Input';
+import { FORMAT_MMSS, stringToTime, timeToString } from '@util/time';
+
+import './countdownStyle';
+
+const DEFAULT_TIME = '05:00';
+
+const Countdown = () => {
+    const [isRunning, setIsRunning] = useState(false);
+    const [time, setTime] = useState(DEFAULT_TIME);
+
+    const buttonLabel = isRunning ? 'End' : 'Start';
+    const buttonVariant = isRunning ? 'danger' : 'success';
+
+    const fTick = () => {
+        const timeAsTime = stringToTime(time, FORMAT_MMSS);
+        const newTime = timeAsTime - 1;
+        const newTimeAsString = timeToString(newTime, FORMAT_MMSS);
+        setTime(newTimeAsString);
+    };
+
+    const onChange = (e) => {
+        const { target: { value } = {} } = e;
+        setTime(value);
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setIsRunning(!isRunning);
+    };
+
+    useEffect(() => {
+        let interval;
+        if (isRunning) {
+            interval = setInterval(fTick, 1000);
+        }
+        return () => clearInterval(interval);
+    }, [isRunning, time]);
+
+    return (
+        <Form className='countdown' onSubmit={onSubmit}>
+            <Row>
+                <Col>
+                    <InputGroup>
+                        <InputGroup.Prepend>
+                            <InputGroup.Text>Duration</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                            disabled={isRunning}
+                            id='duration'
+                            onChange={onChange}
+                            pattern='\d*'
+                            placeholder={DEFAULT_TIME}
+                            type='time'
+                            value={time}
+                        />
+                    </InputGroup>
+                </Col>
+                <Col>
+                    <Button variant={buttonVariant} type='submit'>
+                        {buttonLabel}
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+    );
+};
 
 export default Countdown;
